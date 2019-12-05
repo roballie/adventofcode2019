c9c626c52c8d5a0d8b56fb15bbaa03eb3cc1f6e6
Rob Allie <roballie@gmail.com>
2019-12-05 18:40:15 -0500

    create input parser class

---
+++ adventofcode2019/InputParser.swift
@@ -0,0 +1,13 @@
+//
+//  InputParser.swift
+//  adventofcode2019
+//
+//  Created by Rob Allie on 12/5/19.
+//  Copyright © 2019 Socially Awkward Software. All rights reserved.
+//
+
+import Cocoa
+
+class InputParser {
+
+}
