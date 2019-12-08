c279fb0577d05ccfc0d0ca7d016dfc4ba0f21551
Rob Allie <roballie@gmail.com>
2019-12-08 12:19:38 -0500

    create class for day2

---
+++ adventofcode2019/Day2.swift
@@ -0,0 +1,13 @@
+//
+//  Day2.swift
+//  adventofcode2019
+//
+//  Created by Rob Allie on 12/8/19.
+//  Copyright © 2019 Socially Awkward Software. All rights reserved.
+//
+
+import Cocoa
+
+class Day2 {
+
+}
