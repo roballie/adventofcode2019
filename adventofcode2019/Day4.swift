29664c835ab7251569f455f3f51a548e23ab3b3b
Rob Allie <roballie@gmail.com>
2019-12-10 19:48:26 -0500

    day 4.1 TDD setup

---
+++ adventofcode2019/Day4.swift
@@ -0,0 +1,17 @@
+//
+//  Day4.swift
+//  adventofcode2019
+//
+//  Created by Rob Allie on 12/10/19.
+//  Copyright © 2019 Socially Awkward Software. All rights reserved.
+//
+
+import Cocoa
+
+class Day4 {
+   
+   func doesAdhereToDay4_1Rule(input: Int) -> Bool {
+      return false
+   }
+   
+}
---
+++ adventofcode2019Tests/Day4Tests.swift
@@ -0,0 +1,26 @@
+//
+//  Day4Tests.swift
+//  adventofcode2019Tests
+//
+//  Created by Rob Allie on 12/10/19.
+//  Copyright © 2019 Socially Awkward Software. All rights reserved.
+//
+
+import XCTest
+@testable import adventofcode2019
+
+class Day4Tests: XCTestCase {
+
+   func test1_1() {
+      let input1 = 111111
+      let input2 = 223450
+      let input3 = 123789
+      
+      let day4 = Day4()
+      
+      XCTAssertTrue(day4.doesAdhereToDay4_1Rule(input: input1))
+      XCTAssertFalse(day4.doesAdhereToDay4_1Rule(input: input2))
+      XCTAssertFalse(day4.doesAdhereToDay4_1Rule(input: input3))
+   }
+
+}
